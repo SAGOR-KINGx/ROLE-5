73376d91a8d8a086606431b2b0753f6183297247
JAHIDUL ISLAM SAGOR <jsagor589@gmail.com>
2026-03-13 22:21:32 +0600

    Update tid.js

--- scripts/cmds/tid.js
+++ scripts/cmds/tid.js
@@ -1,21 +1,23 @@
 module.exports = {
-	config: {
-		name: "tid",
-		version: "1.2",
-		author: "NTKhang",
-		countDown: 5,
-		role: 0,
-		description: {
-			vi: "Xem id nhóm chat của bạn",
-			en: "View threadID of your group chat"
-		},
-		category: "info",
-		guide: {
-			en: "{pn}"
-		}
-	},
+  config: {
+    name: "tid",
+    version: "1.0",
+    author: "Jahidul",
+    countDown: 5,
+    role: 0,
+    shortDescription: {
+      en: "Thread ID"
+    },
+    description: {
+      en: "Show only thread ID"
+    },
+    category: "info",
+    guide: {
+      en: "{pn}"
+    }
+  },
 
-	onStart: async function ({ message, event }) {
-		message.reply(event.threadID.toString());
-	}
+  onStart: async function ({ event, message }) {
+    message.reply(event.threadID);
+  }
 };
