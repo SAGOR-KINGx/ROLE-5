3eca9748bd94ec1f5ee37e1bbf5e13f0e7b473cf
JAHIDUL ISLAM SAGOR <jsagor589@gmail.com>
2026-03-14 13:09:24 +0600

    Update onEvent.js

--- scripts/events/onEvent.js
+++ scripts/events/onEvent.js
@@ -4,7 +4,7 @@ module.exports = {
 	config: {
 		name: "onEvent",
 		version: "1.1",
-		author: "NTKhang",
+		author: "SaGor",
 		description: "Loop to all event in global.GoatBot.onEvent and run when have new event",
 		category: "events"
 	},
@@ -12,8 +12,15 @@ module.exports = {
 	onStart: async ({ api, args, message, event, threadsData, usersData, threadModel, userModel, role, commandName }) => {
 		for (const item of allOnEvent) {
 			if (typeof item === "string")
-				continue; // Skip if item is string, because it is the command name and is executed at ../../bot/handler/handlerEvents.js
-			item.onStart({ api, args, message, event, threadsData, usersData, threadModel, userModel, role, commandName });
+				continue;
+
+			try {
+				if (typeof item.onStart === "function") {
+					await item.onStart({ api, args, message, event, threadsData, usersData, threadModel, userModel, role, commandName });
+				}
+			} catch (err) {
+				console.log("onEvent error:", err);
+			}
 		}
 	}
 };
